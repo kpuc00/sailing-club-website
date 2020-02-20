27a9df953a7bba160de63a0a8a7a506856d207fb
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2020-02-20 13:00:27 +0100

    Changed all to .php and trying to minimize code duplication

--- Website/index.php
+++ Website/index.php
@@ -15,37 +15,7 @@
 <body>
 
     <nav>
-        <a class="active" href="index.html"><i class="fa fa-home"></i> Home</a>
-        <a href="about.html"><i class="fa fa-user-circle"></i> About us</a>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <i class="fa fa-anchor"></i> Classes <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href=""><i class="fa fa-ship"></i> Optimist</a>
-                <a href=""><i class="fa fa-ship"></i> Laser</a>
-                <a href=""><i class="fa fa-ship"></i> Finn</a>
-                <a href=""><i class="fa fa-ship"></i> 420</a>
-                <a href=""><i class="fa fa-ship"></i> 470</a>
-            </div>
-        </div>
-        <div class="dropdown">
-            <button class="dropbtn">
-                <i class="fa fa-list"></i> Regattas <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="optimistregatta2019.html"><i class="fa fa-file"></i> Optimist Regatta 2019</a>
-                <a href=""><i class="fa fa-file"></i> Laser 4.7 Regatta 2019</a>
-                <a href=""><i class="fa fa-file"></i> Finn Regatta 2019</a>
-                <a href=""><i class="fa fa-file"></i> 49er Regatta 2019</a>
-                <a href=""><i class="fa fa-file"></i> 420 Regatta 2019</a>
-                <a href=""><i class="fa fa-file"></i> 470 Regatta 2019</a>
-            </div>
-        </div>
-        <div class="contact-button">
-            <a href="contact.html"><i class="fa fa-address-card"></i> Contact</a>
-        </div>
-
+       <?php require_once("navigation_bar.php"); ?>
     </nav>
 
     <div class="content">
