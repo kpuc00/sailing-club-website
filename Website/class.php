125a5361e4ea80dbb9f1556b30488009d265226a
Nikola <n.bolic@student.fontys.nl>
2020-03-08 19:39:42 +0100

    Classes page with missing title and fixed takePartRace css

---
+++ Website/class.php
@@ -0,0 +1,78 @@
+<?php
+require 'php/includes.php';
+?>
+
+<?php 
+    require_once("fetchData/connection.php");
+    require_once("fetchData/GetClassID_Name.php");
+    //$className1 =  $_GET['className'];
+    $classID = $_GET['classID'];
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Class</title>
+    <link rel="stylesheet" type="text/css" href="css/class.css">
+    <link rel="stylesheet" type="text/css" href="css/bodystyle.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
+    <nav>
+        <?php require_once("php/navbar.php"); ?>
+    </nav>
+
+    <div class="class_content">
+        
+        <div class="class_title">
+            <?php echo $classID; ?>
+        </div>
+
+        <div class="class_description">
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, fugiat delectus? Quae in, sunt ab eum eligendi
+                vitae nulla expedita quidem, corporis quibusdam, possimus aperiam distinctio. Corrupti illum quas quasi!
+            </p>
+        </div>
+
+        <div class="timetable">
+            <ul>
+                <li>Monday : 10:00-11:30</li>
+                <li>Tuesday : 10:00-11:30</li>
+                <li>Wednsday : 10:00-11:30</li>
+                <li>Thursday : 10:00-11:30</li>
+                <li>Friday : 10:00-11:30</li>
+                <li>Saturday : 10:00-11:30</li>
+                <li>Sunday : 10:00-11:30</li>
+            </ul>
+        </div>
+        
+        <div class="coach_content">
+
+            <div class="coach_picture">
+                <p>PictureOfCoach</p>
+            </div>
+
+            <div class="coach_description">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, quis voluptate corrupti, 
+                    deserunt voluptas modi esse, voluptatibus eligendi quam perferendis hic itaque porro cupiditate? 
+                    Dolore natus praesentium optio nemo mollitia.
+                </p>
+            </div>
+            
+        </div>
+    </div>
+
+    <footer>
+        <?php require_once("php/footer.php"); ?>
+    </footer>
+
+</body>
+</html>
