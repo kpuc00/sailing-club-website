72eef7abf7982c38aad36f4c96b42b0300fd5f36
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-03-21 01:03:57 +0200

    Updated Login/Register displaying messages

--- Website/auth/auth.php
+++ Website/auth/auth.php
@@ -1,50 +1,48 @@
 <?php
 //database connection
 require 'fetchData/connection.php';
+$message = "";
 
-if ( isset( $_POST['submit'] ) ) { 
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-    exit('Please fill both the username and password fields!');
-}
-//$message = "";
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $conn->prepare('SELECT id, password, displayname FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password, $displayname);
-        $stmt->fetch();
-        // Account exists, now we verify the password.
-        // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if (password_verify($_POST['password'], $password)) {
-            // Verification success! User has loggedin!
-            // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['displayname'] = $displayname;
-            $_SESSION['id'] = $id;
-            //update lastlogin
-            $query = mysqli_query($conn,"UPDATE accounts SET lastlogin = NOW() WHERE username = '".$_SESSION['username']."'");
-            //redirect to homepage
-            header('Location: ../index.php');
-        } else {
-            //echo 'Incorrect password!';
-            $message = "Incorrect password!";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-        }
-    } else {
-        //echo 'Incorrect username/password!';
-        $message = "Incorrect username/password!";
-        echo "<script type='text/javascript'>alert('$message');</script>";
+if (isset($_POST['submit'])) {
+    if (!isset($_POST['username'], $_POST['password'])) {
+        // Could not get the data that should have been sent.
+        exit('Please fill both the username and password fields!');
     }
 
-    $stmt->close();
-}
+    // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+    if ($stmt = $conn->prepare('SELECT id, password, displayname, usertype FROM accounts WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($id, $password, $displayname, $usertype);
+            $stmt->fetch();
+            // Account exists, now we verify the password.
+            // Note: remember to use password_hash in your registration file to store the hashed passwords.
+            if (password_verify($_POST['password'], $password)) {
+                // Verification success! User has loggedin!
+                // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['displayname'] = $displayname;
+                $_SESSION['usertype'] = $usertype;
+                $_SESSION['id'] = $id;
+                //update lastlogin
+                $query = mysqli_query($conn, "UPDATE accounts SET lastlogin = NOW() WHERE username = '" . $_SESSION['username'] . "'");
+                //redirect to homepage
+                header('Location: ../index.php');
+            } else {
+                $message = "Incorrect password!";
+            }
+        } else {
+            $message = "Incorrect username/password!";
+        }
+
+        $stmt->close();
+    }
 }
 ?>
--- Website/register.php
+++ Website/register.php
@@ -1,12 +1,15 @@
 <?php
 require 'php/includes.php';
+require 'auth/registeruser.php';
+
+$msg = $message;
 ?>
 
 <?php
 // If the user is logged in redirect to the index page...
 if (isset($_SESSION['loggedin'])) {
-	header('Location: index.php');
-	exit;
+    header('Location: index.php');
+    exit;
 }
 ?>
 
@@ -15,7 +18,7 @@ if (isset($_SESSION['loggedin'])) {
 
 <head>
     <title>Register</title>
-    <?php require_once("php/head.php");?>
+    <?php require_once("php/head.php"); ?>
     <link rel="stylesheet" type="text/css" href="css/bodystyle.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
@@ -30,25 +33,30 @@ if (isset($_SESSION['loggedin'])) {
 
         <div class="form">
 
-            <form action="auth/registeruser.php" method="post">
-			<h3>Enter your personal data:</h3>
-            
-            <hr>
-            
-            <input type="username" id="username" name="username" placeholder="Username" required>
+            <!--<form action="auth/registeruser.php" method="post">-->
+            <form action="" method="post">
+                <h3>Enter your personal data:</h3>
 
-            <input type="text" id="displayname" name="displayname" placeholder="Display name" required>
+                <div class="error">
+                    <p><?php echo $msg; ?></p>
+                </div>
 
-            <input type="email" id="email" name="email" placeholder="E-mail" required>
-            
-            <input type="password" id="password" name="password" placeholder="Password" required>
+                <hr>
 
-			<!--<input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Confirm password" required>-->
+                <input type="username" id="username" name="username" placeholder="Username" required>
 
-            <hr>
+                <input type="text" id="displayname" name="displayname" placeholder="Display name" required>
 
-            <input type="submit" value="Register">
-            <h3>Already registered? <a href='login.php'>Login</a></h3>
+                <input type="email" id="email" name="email" placeholder="E-mail" required>
+
+                <input type="password" id="password" name="password" placeholder="Password" required>
+
+                <!--<input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Confirm password" required>-->
+
+                <hr>
+
+                <input type="submit" name="submit" value="Register">
+                <h3>Already registered? <a href='login.php'>Login</a></h3>
 
             </form>
         </div>
