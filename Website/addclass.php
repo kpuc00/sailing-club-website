bc44f3c815b9342e247aea69aa7c7d9434e65d37
Nikola <n.bolic@student.fontys.nl>
2020-03-21 15:00:22 +0100

    addClass js validation added

--- Website/addclass.php
+++ Website/addclass.php
@@ -24,13 +24,13 @@
             <hr>
             
             <input type="title" id="className" name="className" placeholder="Class Name"> 
-
+            <div id="classNameError"></div>
             <textarea id="subject" id="classDescription" name="classDescription" placeholder="Class Description" style="height:200px"></textarea>
-            
+            <div id="classDescriptionError"></div>
             <br>
             <p>Choose picture</p>
             <input type="file" id="classPicture" name="file" accept="image/x-png,image/gif,image/jpeg"> 
-        
+            <div id="classPictureError"></div>
 
             <hr>
 
